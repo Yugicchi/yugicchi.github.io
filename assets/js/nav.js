bf744734d7c65ead9426fa38cb22ee8b8f503768
Yuginara Davin K. <74823454+Yugicchi@users.noreply.github.com>
2025-01-28 17:39:23 +0700

    navigation

---
+++ assets/js/nav.js
@@ -0,0 +1,6 @@
+<script>
+  document.getElementById("logo-link").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "../index.html";
+  });
+</script>
