64ba0d9d48b8af3c25954cb487f58067ecb1afd9
Yuginara Davin K. <74823454+Yugicchi@users.noreply.github.com>
2025-01-28 18:01:01 +0700

    forgor 💀

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,2 @@
 <script>
-  document.getElementById("logo-link").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../index.html";
-  });
 </script>
